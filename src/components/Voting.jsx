f6cd41679acc5fbdeb348bcc90c4cc2b8853492d
LukeGGChapman <lukeggchapman@gmail.com>
2015-12-16 10:19:20 +1100

    Added voting and winner components

---
+++ src/components/Voting.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import Winner from './Winner';
+import Vote from './Vote';
+
+export default React.createClass({
+  mixins: [PureRenderMixin],
+
+  render: function () {
+    return <div className="voting">
+      {this.props.winner ?
+        <Winner ref="winner" winner={this.props.winner} /> :
+        <Vote {...this.props} />}
+    </div>;
+  }
+});
