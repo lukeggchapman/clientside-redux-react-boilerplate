74238367bf70aed3b64dff6e7cc8d0635e2610ab
LukeGGChapman <lukeggchapman@gmail.com>
2015-12-16 10:10:41 +1100

    Replaced jsdom with testdom, fixed jsx test watcher

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,17 +1,7 @@
-import jsdom from 'jsdom';
+import testDom from 'testdom';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
 
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
-const win = doc.defaultView;
-
-global.document = doc;
-global.window = win;
-
-Object.keys(window).forEach((key) => {
-  if (!(key in global)) {
-    global[key] = window[key];
-  }
-});
+const doc = testDom('<!doctype html><html><body></body></html>');
 
 chai.use(chaiImmutable);
