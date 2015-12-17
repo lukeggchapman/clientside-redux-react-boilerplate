542c733a914ca82257efa0831e372c9381ce285a
LukeGGChapman <lukeggchapman@gmail.com>
2015-12-17 12:23:51 +1100

    Completed UI modules

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router, {Route} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
-const pair = ['Trainspotting', '28 Days Later'];
+const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
+  <Route path="/" component={Voting} />
+</Route>;
 
 ReactDOM.render(
-  <Voting pair={pair} winner="Trainspotting" />,
+  <Router>{routes}</Router>,
   document.getElementById('app')
 );
